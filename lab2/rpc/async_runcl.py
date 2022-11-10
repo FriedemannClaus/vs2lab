34fc002c46f135352f010f467b7b726ff4bfa937
FriedemannClaus <FriedemannClaus@users.noreply.github.com>
2022-11-10 13:27:10 +0100

    Lösung Lab2

---
+++ lab2/rpc/async_runcl.py
@@ -0,0 +1,21 @@
+import async_rpc as rpc
+from context import lab_logging
+import logging
+
+lab_logging.setup(stream_level=logging.INFO)
+
+def callback(res_list):
+    print("Result: {}".format(res_list.value))
+
+cl = rpc.Client()
+cl.run()
+
+base_list = rpc.DBList({'foo'})
+cl.append("bar", base_list, callback)
+cl.printMsgs()
+
+cl.thread.join()
+
+print("\nClient process finished")
+
+cl.stop()
